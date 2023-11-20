343daaf6587370f8072bece0da0171f156081e0f
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-11-20 13:06:26 -0800

    created test specs for get route

--- index.test.js
+++ index.test.js
@@ -12,7 +12,12 @@ const seedMusician = require("./seedData");
 
 describe('./musicians endpoint', () => {
     // Write your tests here
-    
+    test("Brings back all musicians",async() =>{
+        const response = await request(app).get("/musicians")
+        const responseData = JSON.parse(response.text)
+        expect(response.statusCode).toBe(200)
+        expect(responseData[0].id).toBe(1)
+    })
     
 
 
